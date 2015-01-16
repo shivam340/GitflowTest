d8cfbad328b4d5b0b7f2310c6bb0eceb5a32763e
shivam gosavi <shivam.gosavi680@gmail.com>
2015-01-16 14:20:32 +0530

    feature added in develop

--- app/src/main/java/shivam/com/gitflowtest/MainActivity.java
+++ app/src/main/java/shivam/com/gitflowtest/MainActivity.java
@@ -4,15 +4,18 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
 
-    private Button mBtnClick;
+    private Button mBtnClick, mBtnClick2;
     private TextView mTxtMessage;
-
+    private EditText mEdtNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,11 +56,61 @@ public class MainActivity extends ActionBarActivity {
      */
     private void initUi() {
 
-        mBtnClick = (Button) findViewById(R.id.btn_click_here);
+        mBtnClick = (Button) findViewById(R.id.btn_click_here1);
+        mBtnClick2 = (Button) findViewById(R.id.btn_click_here2);
         mTxtMessage = (TextView) findViewById(R.id.txt_message);
+        mEdtNumber = (EditText) findViewById(R.id.edt_number);
 
+        mBtnClick.setOnClickListener(new HandleOnClick(R.id.btn_click_here1));
+        mBtnClick2.setOnClickListener(new HandleOnClick(R.id.btn_click_here2));
     }
 
 
+    private class HandleOnClick implements View.OnClickListener  {
+
+        private int mId;
+
+        public HandleOnClick (int id) {
+
+            mId = id;
+        }
+
+        @Override
+        public void onClick(View view) {
+
+
+            switch(mId) {
+
+                case R.id.btn_click_here1:
+
+                    Toast.makeText(getApplicationContext(),"on click1", Toast.LENGTH_SHORT).show();
+                    break;
+
+                case R.id.btn_click_here2:
+
+                        if( mEdtNumber.getText() != null && mEdtNumber.getText().toString().trim().length()>0 ) {
+
+                            try {
+                                int number = Integer.parseInt(mEdtNumber.getText().toString().trim());
+                                Toast.makeText(getApplicationContext(), "done", Toast.LENGTH_SHORT).show();
+                            }
+                            catch(NumberFormatException ex) {
+
+                                Toast.makeText(getApplicationContext(), "please enter a valid number ", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    else {
+
+                            Toast.makeText(getApplicationContext(), " insufficient information.", Toast.LENGTH_SHORT).show();
+                        }
+
+                    break;
+
+
+            }
+
+        }
+
+    }
 
 }
